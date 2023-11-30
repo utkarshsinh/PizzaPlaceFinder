1d0d58fbe723605f4bc5ab714741eed9d975cc23
utkarshsinh <usinh002@ucr.edu>
2023-11-29 18:56:14 -0800

    Push

---
+++ stores.js
@@ -0,0 +1,350 @@
+const storeList = [
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-76.6412712, 39.0457549]
+        },
+        "properties": {
+          "name": "Bella Pizza Joint",
+          "address": "886 Kevin Divide, Taraland, MD 18132",
+          "phone": "(231) 933-1484"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-115.7381, 40.1049]
+        },
+        "properties": {
+          "name": "Sourdough Pizza Bistro",
+          "address": "443 Nicole Path Suite 041, Barreraview, RI 89204",
+          "phone": "(213) 311-9490"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-75.5276699, 38.9108325]
+        },
+        "properties": {
+          "name": "Mama Mia's Pizza Place",
+"address": "46766 Adkins Isle Apt. 572, North Natalieburgh, DE 58314",
+"phone": "(717) 319-9514"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-109.7381, 43.1049]
+        },
+        "properties": {
+          "name": "Tony's Pizza Oven",
+"address": "PSC 1386, Box 1350, APO AP 92976",
+"phone": "(278) 743-3106"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-74.0059728, 40.7127753]
+        },
+        "properties": {
+          "name": "Capri Pizza Corner",
+          "address": "82186 Jarvis Island, West Stacy, NY 20336",
+          "phone": "(955) 921-9186"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-87.7381, 42.6049]
+        },
+        "properties": {
+          "name": "Vesuvio Pizza Kitchen",
+"address": "928 Wood Square, Hamiltonhaven, NJ 28431",
+"phone": "(888) 688-5245"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-105.3806, 36.5400]
+        },
+        "properties": {
+          "name": "Vito's Pizza Corner",
+"address": "Unit 7276 Box 7997, DPO AE 46769",
+"phone": "(582) 559-4577"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-88.7417, 42.6362]
+        },
+        "properties": {
+          "name": "Roma Pizza Oven",
+"address": "Unit 4400 Box 5127, DPO AE 46543",
+"phone": "(483) 244-1522"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-81.3908, 40.7025]
+        },
+        "properties": {
+          "name": "Ray's Pizzeria",
+"address": "468 Kevin Crossroad, Cristinaton, IN 81512",
+"phone": "(457) 984-3306"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-107.7551, 29.7246]
+        },
+        "properties": {
+          "name": "Capri Pizza Bistro",
+"address": "PSC 1560, Box 9960, APO AP 73301",
+"phone": "(947) 555-9904"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-112.7381, 41.1049]
+        },
+        "properties": {
+          "name": "Gino's Pizza Joint",
+"address": "2230 Ward Squares, Port Kathleen, TN 06831",
+"phone": "(879) 245-2999"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-115.8953, 35.1815]
+        },
+        "properties": {
+          "name": "Napoli Pizza",
+"address": "346 Larry Plains, Mooreton, SD 96536",
+          "phone": "(388) 594-9600"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-106.0097, 27.9295]
+        },
+        "properties": {
+          "name": "Pizzeria Da Marco",
+"address": "980 Liberty Avenue, Boston, MA 02109",
+"phone": "(617) 555-0132"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-91.9664, 39.1992]
+        },
+        "properties": {
+          "name": "Pizza Guys",
+"address": "8710 19th St #100, Rancho Cucamonga, CA 91701",
+"phone": "(909) 481-4747"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-85.7995, 32.8545]
+        },
+        "properties": {
+          "name": "Sorrento's Pizza Haven",
+"address": "1208 Maple Avenue, Springfield, IL 62704",
+"phone": "(217) 555-0192"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-97.7381, 41.1049]
+        },
+        "properties": {
+          "name": "Pisa Pizza Tower",
+"address": "678 Lincoln Avenue, Chicago, IL 60607",
+"phone": "(312) 555-0133"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-107, 46.1049]
+        },
+        "properties": {
+          "name": "Giovanni's Pizza Emporium",
+"address": "482 Oak Street, Phoenix, AZ 85003",
+"phone": "(602) 555-0134"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-107.7381, 44.1049]
+        },
+        "properties": {
+          "name": "Roma's Pizza Retreat",
+"address": "33 Elm Street, Denver, CO 80203",
+"phone": "(303) 555-0789"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-74.0063, 41.9983]
+        },
+        "properties": {
+          "name": "Napoli's Pizza Corner",
+"address": "256 Cedar Lane, Portland, OR 97205",
+"phone": "(971) 555-0225"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-107.7690, 26.8704]
+        },
+        "properties": {
+          "name": "Sicily's Slice of Heaven",
+"address": "1203 Sunset Blvd, Los Angeles, CA 90026",
+"phone": "(213) 555-0229"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-88.3381, 45.7072]
+        },
+        "properties": {
+          "name": "Calabria Pizza Kitchen",
+"address": "990 Main Street, Columbus, OH 43215",
+"phone": "(614) 555-0184"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-97.7381, 49.1049]
+        },
+        "properties": {
+          "name": "Positano's Pizza Place",
+"address": "433 Elm Drive, Houston, TX 77002",
+"phone": "(713) 555-0201"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-87.7806, 36.2689]
+        },
+        "properties": {
+          "name": "Diavolo's Pizza Den",
+"address": "771 Maple Avenue, Las Vegas, NV 89101",
+"phone": "(702) 555-0153"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-91.7988, 31.0970]
+        },
+        "properties": {
+          "name": "Venice Pizza Haven",
+"address": "208 Pine Street, Orlando, FL 32801",
+"phone": "(407) 555-0266"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-88.8946, 44.9217]
+        },
+        "properties": {
+          "name": "Amalfi Coast Pizzeria",
+"address": "1320 Broadway Blvd, Kansas City, MO 64105",
+"phone": "(816) 555-0110"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-120.1577, 35.1819]
+        },
+        "properties": {
+          "name": "Tuscany Pizza Bistro",
+"address": "67 Walnut Avenue, Charlotte, NC 28202",
+"phone": "(704) 555-0147"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-89.6322, 40.8074]
+        },
+        "properties": {
+          "name": "Pompeii's Pizza Parlor",
+"address": "890 Market Street, Philadelphia, PA 19107",
+"phone": "(215) 555-0321"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-113.8505, 46.0649]
+        },
+        "properties": {
+          "name": "Fiore's Pizza Garden",
+"address": "324 Oak Street, Nashville, TN 37203",
+"phone": "(615) 555-0204"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [-109.7381, 43.1049]
+        },
+        "properties": {
+          "name": "MArcello's Pizza",
+          "address": "783 W Blaine St, Riverside, CA 92507",
+          "phone": "(951) 781-9996"
+        }
+      }
+]
